570e8a9f63ad11f3dc7e8b70fb3539c1f54a96ef
Pouya Kary <pmk@nobody.invalid>
2018-02-26 00:57:16 +0330

    code prettier

--- source/main.ts
+++ source/main.ts
@@ -16,9 +16,7 @@ namespace Shapes {
 
         window.onload = ( ) => {
             Storage.initStorage( )
-
             StateManipulotrs.init( )
-
             MouseDriver.init( )
             ScreenDriver.init( )
 
